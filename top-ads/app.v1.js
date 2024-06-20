51d0875e7f72ed5890fbfcc444fe9f15b55f7779
aihris <pelisford@gmail.com>
2024-06-20 18:17:32 -0500

    Add files via upload

---
+++ top-ads/app.v1.js
@@ -0,0 +1,29 @@
+function loadTopAds(url) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = url;
+    document.head.appendChild(link);
+}
+
+loadTopAds('app.v1.css');
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+    const ads = document.querySelectorAll('.floating-top-ads');
+
+    if (scrollPosition > 100) {
+        ads.forEach(ad => {
+            ad.classList.add('visible-top-ads');
+        });
+    } else {
+        ads.forEach(ad => {
+            ad.classList.remove('visible-top-ads');
+        });
+    }
+});
+
+document.querySelectorAll('.close-top-ads').forEach(button => {
+    button.addEventListener('click', () => {
+        button.parentElement.style.display = 'none';
+    });
+});
