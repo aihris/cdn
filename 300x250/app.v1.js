c8bad564441202df03443a17248f3281e939a1f9
aihris <pelisford@gmail.com>
2024-06-21 16:28:25 -0500

    Add files via upload

---
+++ 300x250/app.v1.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    function loadTopAds(addCss300x250) {
+        const linkCss300x250 = document.createElement('link');
+        linkCss300x250.rel = 'stylesheet';
+        linkCss300x250.href = addCss300x250;
+        document.head.appendChild(linkCss300x250);
+    }
+
+    loadTopAds('app.v1.css');
+
+    const adFloating300x250 = document.getElementById('floating-ad-300x250');
+
+    const closeBt300x250 = document.createElement('div');
+    closeBt300x250.id = 'close-btn-300x250';
+    closeBt300x250.onclick = function () {
+        adFloating300x250.style.opacity = '0';
+        adFloating300x250.style.transform = 'scale(0.9)';
+        setTimeout(function () {
+            adFloating300x250.remove();
+        }, 500);
+    };
+    adFloating300x250.appendChild(closeBt300x250);
+
+    setTimeout(function () {
+        adFloating300x250.classList.add('visible-ad-300x250');
+    }, 100);
+});
